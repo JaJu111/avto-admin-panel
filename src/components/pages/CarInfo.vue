c58d9ab2faef6fd545b567fedb996d5066b79f82
Jaloliddin Musabayev <musaboyev6@gmail.com>
2024-10-28 17:48:55 +0500

    fixed errors

--- src/components/pages/CarInfo.vue
+++ src/components/pages/CarInfo.vue
@@ -128,7 +128,6 @@ export default class CarInfo extends Vue {
 
 .car-info-img-box
     display: flex
-    justify-content: center
     width: 100%
     margin-bottom: 32px
 
@@ -136,8 +135,8 @@ export default class CarInfo extends Vue {
         display: flex
         justify-content: center
         align-items: center
-        width: 500px
-        height: 250px
+        width: 600px
+        height: auto
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1)
         overflow: hidden
 
